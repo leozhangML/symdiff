c9f9e719d6bc9a56c198d6b9fb75001969ca15e1
kiaashour <kianooshashouritaklimi@yahoo.co.uk>
2024-09-09 15:47:28 +0100

    Update bash script

--- train_dit_dit_gaussian_6.sh
+++ train_dit_dit_gaussian_6.sh
@@ -4,8 +4,13 @@
 #SBATCH --output=/tmp/slurm-%j.out
 #SBATCH --error=/tmp/slurm-%j.out
 
+
+# Emails
+#SBATCH --mail-user=kianoosh.ashouritaklimi@stats.ox.ac.uk
+#SBATCH --mail-type=ALL
+
 # Name of job
-#SBATCH --job-name=DiT_DiTGaussian_SwiGLU_test
+#SBATCH --job-name=main_test
 
 # Using thet cluster srf_gpu_01 and node 6
 #SBATCH --cluster=srf_gpu_01
@@ -13,7 +18,7 @@
 #SBATCH --gres=gpu:1
 
 # Change if you know what doing (look at examples, notes)
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=1
 
 # This is useful for selecting the particular nodes that you want
 #NOTSBATCH --nodelist=zizgpu06.cpu.stats.ox.ac.uk
@@ -26,12 +31,10 @@
 #SBATCH --ntasks=1
 
 
-echo "bruh"
+export WANDB_API_KEY="b251e22c374eb363aa6189f343b015bbbeb6bad7"
+export PATH_TO_CONDA="/data/localhost/not-backed-up/$USER/miniforge3"
+source $PATH_TO_CONDA/bin/activate symdif
 
-export WANDB_API_KEY="78d61cd721affd9ffa2f5e217ed6f49de71eb842"
-
-export PATH_TO_CONDA="/data/localhost/not-backed-up/$USER/miniconda3"
-source $PATH_TO_CONDA/bin/activate symdiff_env
 
 # Create central and local logging directories
 export LOCAL_LOGDIR="/data/localhost/not-backed-up/$USER/symdiff"
@@ -39,20 +42,20 @@ mkdir -p $LOCAL_LOGDIR/output
 mkdir -p $LOCAL_LOGDIR/slurm
 
 # The central logdir could be in /data/ziz/$USER or /data/ziz/not-backed-up/$USER instead of /data/ziz/not-backed-up/scratch/
-export CENTRAL_LOGDIR="/data/ziz/not-backed-up/$USER/projects/symdiff"
+export CENTRAL_LOGDIR="/data/ziz/not-backed-up/$USER/symdiff"
 mkdir -p $CENTRAL_LOGDIR
 mkdir -p $CENTRAL_LOGDIR/outputs
 mkdir -p $CENTRAL_LOGDIR/outputs/slurm
 
 echo "SLURM_JOBID: " $SLURM_JOBID
-echo "bruh"
 date -u
 
-python main_qm9.py --exp_name DiT_DiTGaussian_test --model dit_dit_gaussian_dynamics --dataset qm9 --datadir /data/zizgpu06/not-backed-up/nvme00/lezhang \
+
+python main_qm9.py --exp_name DiT_DiTGaussian_test --model dit_dit_gaussian_dynamics --dataset qm9 --datadir /data/localhost/not-backed-up/nvme00/ashouritaklimi \
                    --diffusion_noise_precision 1e-5 --diffusion_steps 1000 --diffusion_loss_type l2 --diffusion_noise_schedule polynomial_2 \
                    --n_epochs 5000 --batch_size 256 --lr 1e-4 --com_free --clipping_type norm --max_grad_norm 2.0 --ema_decay 0.9999 \
                    --weight_decay 1e-12 --use_amsgrad --normalize_factors [1,4,10] \
-                   --n_stability_samples 500 --test_epochs 20 --wandb_usr zhangleo1209 --save_model True \
+                   --n_stability_samples 500 --test_epochs 20 --wandb_usr kiaashouri --save_model True \
                    --xh_hidden_size 184 --K 184 \
                    --mlp_type swiglu \
                    --enc_hidden_size 128 --enc_depth 8 --enc_num_heads 4 --enc_mlp_ratio 4.0 --dec_hidden_features 64 \
@@ -61,9 +64,5 @@ python main_qm9.py --exp_name DiT_DiTGaussian_test --model dit_dit_gaussian_dyna
                    --print_parameter_count 
 
 date -u
-
-# script to run main.py
 echo "Job completed."
-#cp /tmp/slurm-${SLURM_JOB_ID}.out ${LOCAL_LOGDIR}/slurm/.
 cp /tmp/slurm-${SLURM_JOB_ID}.out ${CENTRAL_LOGDIR}/outputs/slurm/.
-#cp -r $LOCAL_LOGDIR/output/* $CENTRAL_LOGDIR/output/.
