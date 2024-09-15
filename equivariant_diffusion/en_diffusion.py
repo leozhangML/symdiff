fixed ve scaling, added 2D experiments, cleaning up main_qm9.py args

equivariant_diffusion/en_diffusion.py
@@ -226,7 +226,7 @@ class VENoiseSchedule(torch.nn.Module):
             torch.zeros([timesteps+1]),
             requires_grad=False
         )
-        # need to use timesteps+1??? as we sample in {0, ..., T}
+        # need to use timesteps+1 as we sample in {0, ..., T}
         self.log_sigmas2 = torch.nn.Parameter(
             torch.log(
                 ve_schedule(timesteps+1, rho=rho, 
@@ -326,9 +326,9 @@ class EnVariationalDiffusion(torch.nn.Module):
         # The network that will predict the denoising.
         self.dynamics = dynamics
 
-        self.in_node_nf = in_node_nf  # atom decoder \pm include_charge
+        self.in_node_nf = in_node_nf  # atom decoder \pm include_charge; 1 for toy experiments
         self.n_dims = n_dims
-        self.num_classes = self.in_node_nf - self.include_charges  # 'atom_decoder': ['H', 'C', 'N', 'O', 'F'] where does charges go
+        self.num_classes = self.in_node_nf - self.include_charges if self.molecule else 5  # 'atom_decoder': ['H', 'C', 'N', 'O', 'F'] where does charges go
 
         self.T = timesteps
         self.parametrization = parametrization
@@ -343,7 +343,7 @@ class EnVariationalDiffusion(torch.nn.Module):
     def check_issues_norm_values(self, num_stdevs=8):  # need differences in discrete values to be large compared to \sigma_0
         zeros = torch.zeros((1, 1))
         gamma_0 = self.gamma(zeros)
-        sigma_0 = self.sigma(gamma_0, target_tensor=zeros).item()
+        sigma_0 = self.sigma(gamma_0, target_tensor=zeros).item()  # [1, 1]
 
         # Checked if 1 / norm_value is still larger than 10 * standard
         # deviation.
@@ -359,7 +359,7 @@ class EnVariationalDiffusion(torch.nn.Module):
         if self.com_free:
             net_out = self.dynamics._forward(t, x, node_mask, edge_mask, context)
         else:
-            net_out = self.dynamics._forward(t, x, node_mask, edge_mask, context, gamma=self.gamma(t))
+            net_out = self.dynamics._forward(t, x, node_mask, edge_mask, context, gamma_t=self.gamma(t))
         return net_out
 
     def inflate_batch_array(self, array, target):
@@ -467,7 +467,10 @@ class EnVariationalDiffusion(torch.nn.Module):
 
         else:
             bs = len(gamma_t[0])
-            alpha_t_given_s = self.inflate_batch_array(torch.ones([bs], device=target_tensor.device), target_tensor)
+            alpha_t_given_s = self.inflate_batch_array(
+                torch.ones([bs], device=target_tensor.device), 
+                target_tensor
+            )
             sigma2_t_given_s = self.inflate_batch_array(
                 torch.exp(gamma_t[1]) - torch.exp(gamma_s[1]),
                 target_tensor
@@ -492,7 +495,7 @@ class EnVariationalDiffusion(torch.nn.Module):
         mu_T_x, mu_T_h = mu_T[:, :, :self.n_dims], mu_T[:, :, self.n_dims:]
 
         # Compute standard deviations (only batch axis for x-part, inflated for h-part).
-        sigma_T_x = self.sigma(gamma_T, mu_T_x).squeeze()  # NOTE: Remove inflate, only keep batch dimension for x-part. - #[bs, 1]
+        sigma_T_x = self.sigma(gamma_T, mu_T_x).squeeze()  # NOTE: Remove inflate, only keep batch dimension for x-part. - # [bs, 1]
         sigma_T_h = self.sigma(gamma_T, mu_T_h)  # [bs, 1, 1]
 
         if self.com_free:
@@ -537,9 +540,18 @@ class EnVariationalDiffusion(torch.nn.Module):
 
     def compute_error(self, net_out, gamma_t, eps):
         """Computes error, i.e. the most likely prediction of x."""
-        eps_t = net_out
+
+        if self.molecule:
+            eps_t = net_out
+            in_node_nf = self.in_node_nf
+        else:
+            # computes error just for x 
+            eps_t = net_out[:, :, :self.n_dims]
+            in_node_nf = 0
+            eps = eps[:, :, :self.n_dims]
+
         if self.training and self.loss_type == 'l2':
-            denom = (self.n_dims + self.in_node_nf) * eps_t.shape[1]
+            denom = (self.n_dims + in_node_nf) * eps_t.shape[1]
             error = sum_except_batch((eps - eps_t) ** 2) / denom
         else:
             error = sum_except_batch((eps - eps_t) ** 2)  # [bs, n_nodes, dim] to [bs]
@@ -582,7 +594,7 @@ class EnVariationalDiffusion(torch.nn.Module):
             sigma_x = self.SNR(-0.5 * gamma_0).unsqueeze(1)  # [bs]
         else:
             sigma_x = torch.exp(0.5 * gamma_0[1]).unsqueeze(1)
-        net_out = self.phi(z0, zeros, node_mask, edge_mask, context)  # [bs, n_nodes, dims]
+        net_out = self.phi(z0, zeros, node_mask, edge_mask, context)  # [bs, n_nodes, dims] - should also include h?
 
         # Compute mu for p(zs | zt).
         mu_x = self.compute_x_pred(net_out, z0, gamma_0)
@@ -667,10 +679,12 @@ class EnVariationalDiffusion(torch.nn.Module):
         # Combine categorical and integer log-probabilities.
         log_p_h_given_z = log_ph_integer + log_ph_cat
 
-        # Combine log probabilities for x and h.
-        log_p_xh_given_z = log_p_x_given_z_without_constants + log_p_h_given_z
-
-        return log_p_xh_given_z
+        if self.molecule:
+            # Combine log probabilities for x and h.
+            log_p_xh_given_z = log_p_x_given_z_without_constants + log_p_h_given_z
+            return log_p_xh_given_z
+        else:
+            return log_p_x_given_z_without_constants
 
     def compute_loss(self, x, h, node_mask, edge_mask, context, t0_always):
         """Computes an estimator for the variational lower bound, or the simple loss (MSE)."""
@@ -818,6 +832,9 @@ class EnVariationalDiffusion(torch.nn.Module):
             loss, loss_dict = self.compute_loss(x, h, node_mask, edge_mask, context, t0_always=True)
 
         neg_log_pxh = loss
+
+        if not self.molecule:
+            delta_log_px = torch.zeros_like(delta_log_px)
 
         # Correct for normalization on x.
         assert neg_log_pxh.size() == delta_log_px.size()  # np scalar

main_qm9.py
@@ -146,121 +146,34 @@ parser.add_argument("--sigma_max", type=float, default=10, help="VE schedule")
 parser.add_argument("--print_grad_norms", action="store_true", help="whether to show the gamma and k grad norms")
 parser.add_argument("--print_parameter_count", action="store_true", help="whether to show the gamma and k param count")
 
-# -------- sym_diff perceiver args -------- #
+# -------- DiTGaussian args -------- #
 
-parser.add_argument("--context_hidden_size", type=int, default=512, help="preprocessor config for perceiver")
+parser.add_argument("--xh_hidden_size", type=int, default=128, help="config for DiTGaussian")
+parser.add_argument("--K", type=int, default=128, help="config for DiTGaussian")
+parser.add_argument("--pos_embedder_test", type=int, default=4, help="config for DiTGaussian")
 
-parser.add_argument("--gamma_num_latents", type=int, default=64, help="gamma config for perceiver")
-parser.add_argument("--gamma_d_latents", type=int, default=128, help="gamma config for perceiver")
-parser.add_argument("--gamma_n_pad", type=int, default=61, help="gamma config for perceiver")
-parser.add_argument("--gamma_num_blocks", type=int, default=1, help="gamma config for perceiver")
-parser.add_argument("--gamma_num_self_attends_per_block", type=int, default=3, help="gamma config for perceiver")
-parser.add_argument("--gamma_num_self_attention_heads", type=int, default=4, help="gamma config for perceiver")
-parser.add_argument("--gamma_num_cross_attention_heads", type=int, default=4, help="gamma config for perceiver")
-parser.add_argument("--gamma_attention_probs_dropout_prob", type=float, default=0.1, help="gamma config for perceiver")
-parser.add_argument("--gamma_pos_num_channels", type=int, default=64, help="gamma config for perceiver")
-parser.add_argument("--gamma_num_heads", type=int, default=4, help="gamma config for perceiver")
-
-parser.add_argument("--k_num_latents", type=int, default=128, help="k config for perceiver")
-parser.add_argument("--k_d_latents", type=int, default=256, help="k config for perceiver")
-parser.add_argument("--k_n_pad", type=int, default=55, help="k config for perceiver")
-parser.add_argument("--k_num_blocks", type=int, default=1, help="k config for perceiver")
-parser.add_argument("--k_num_self_attends_per_block", type=int, default=10, help="k config for perceiver")
-parser.add_argument("--k_num_self_attention_heads", type=int, default=4, help="k config for perceiver")
-parser.add_argument("--k_num_cross_attention_heads", type=int, default=4, help="k config for perceiver")
-parser.add_argument("--k_attention_probs_dropout_prob", type=float, default=0.1, help="k config for perceiver")
-parser.add_argument("--k_enc_mlp_factor", type=int, default=2, help="k config for perceiver")
-
-parser.add_argument("--k_pos_num_channels", type=int, default=64, help="k config for perceiver")
-parser.add_argument("--k_num_heads", type=int, default=4, help="k config for perceiver")
-parser.add_argument("--k_decoder_self_attention", action="store_true", help="k config for perceiver")
-parser.add_argument("--k_num_self_heads", type=int, default=4, help="k config for perceiver")
-parser.add_argument("--k_query_residual", action="store_true", help="k config for perceiver")
-
-parser.add_argument("--decoder_hidden_size", type=int, default=256, help="k config for perceiver")
-
-# -------- sym_diff transformer args -------- #
-
-parser.add_argument("--gamma_num_enc_layers", type=int, default=2, help="gamma config for transformer")
-parser.add_argument("--gamma_num_dec_layers", type=int, default=2, help="gamma config for transformer")
-parser.add_argument("--gamma_d_model", type=int, default=128, help="gamma config for transformer")
-parser.add_argument("--gamma_nhead", type=int, default=4, help="gamma config for transformer")
-parser.add_argument("--gamma_dim_feedforward", type=int, default=256, help="gamma config for transformer")
-parser.add_argument("--gamma_dropout", type=float, default=0.1, help="gamma config for transformer")
-
-parser.add_argument("--k_num_layers", type=int, default=6, help="k config for transformer")
-parser.add_argument("--k_d_model", type=int, default=256, help="k config for transformer")
-parser.add_argument("--k_nhead", type=int, default=8, help="k config for transformer")
-parser.add_argument("--k_dim_feedforward", type=int, default=512, help="k config for transformer")
-parser.add_argument("--k_dropout", type=float, default=0.1, help="k config for transformer")
-
-# -------- sym_diff perceiver fourier args -------- #
-
-parser.add_argument("--sigma", type=float, default=100, help="config for perceiver fourier")
-parser.add_argument("--m", type=int, default=20, help="config for perceiver fourier")
-
-# -------- perceiver_gaussian args -------- #
-
-parser.add_argument("--pos_emb_size", type=int, default=256, help="config for perceiver fourier")
-parser.add_argument("--k_mlp_factor", type=int, default=2, help="config for perceiver fourier")
-
-# -------- transformer args -------- #
-
-parser.add_argument("--trans_num_layers", type=int, default=6, help="config for transformer")
-parser.add_argument("--trans_d_model", type=int, default=256, help="config for transformer")
-parser.add_argument("--trans_nhead", type=int, default=8, help="config for transformer")
-parser.add_argument("--trans_dim_feedforward", type=int, default=512, help="config for transformer")
-parser.add_argument("--trans_dropout", type=float, default=0., help="config for transformer")
-
-# -------- DiT args -------- #
-
-parser.add_argument("--out_channels", type=int, default=9, help="config for DiT")
-parser.add_argument("--x_scale", type=float, default=25.0, help="config for DiT")
 parser.add_argument("--hidden_size", type=int, default=256, help="config for DiT")
 parser.add_argument("--depth", type=int, default=6, help="config for DiT")
 parser.add_argument("--num_heads", type=int, default=4, help="config for DiT")
 parser.add_argument("--mlp_ratio", type=float, default=2.0, help="config for DiT")
-parser.add_argument("--subtract_x_0", action="store_true", help="config for DiT")
+parser.add_argument("--mlp_dropout", type=float, default=0.0, help="config for DiTEmb")
 
-parser.add_argument("--x_emb", type=str, default="fourier", help="config for DiT")
 
-# -------- DiT_GNN and DiT_DiT args -------- #
+# -------- DiT_DiTGaussian args -------- #
 
-parser.add_argument("--enc_out_channels", type=int, default=1, help="config for DiT_GNN")  # not used
-parser.add_argument("--enc_x_scale", type=float, default=25.0, help="config for DiT_GNN")
 parser.add_argument("--enc_hidden_size", type=int, default=64, help="config for DiT_GNN")
 parser.add_argument("--enc_depth", type=int, default=4, help="config for DiT_GNN")
 parser.add_argument("--enc_num_heads", type=int, default=4, help="config for DiT_GNN")
 parser.add_argument("--enc_mlp_ratio", type=float, default=4, help="config for DiT_GNN")
+
 parser.add_argument("--dec_hidden_features", type=int, default=32, help="config for DiT_GNN")
 
-parser.add_argument("--enc_x_emb", type=str, default="linear", help="config for DiT_GNN and DiT_DiT")
 parser.add_argument("--enc_concat_h", action="store_true", help="config for DiT_GNN")
+
 parser.add_argument("--noise_dims", type=int, default=16, help="config for DiT_GNN")
 parser.add_argument("--noise_std", type=float, default=1.0, help="config for DiT_GNN")
 
-# -------- DiTGaussian_GNN args -------- #
-
-parser.add_argument("--pos_size", type=int, default=128, help="config for DiTGaussian_GNN")
-
-# -------- GNN_DiT args -------- #
-
-parser.add_argument("--gamma_gnn_layers", type=int, default=4, help="config for GNN_GNN")
-parser.add_argument("--gamma_gnn_hidden_size", type=int, default=64, help="config for GNN_GNN")
-parser.add_argument("--gamma_gnn_out_size", type=int, default=64, help="config for GNN_GNN")
-parser.add_argument("--gamma_dec_hidden_size", type=int, default=32, help="config for GNN_GNN")
-
-# -------- DiTEmb args -------- #
-
-parser.add_argument("--xh_hidden_size", type=int, default=128, help="config for DiTEmb")
-parser.add_argument("--mlp_dropout", type=float, default=0.0, help="config for DiTEmb")
-
-# -------- DiTGaussian args -------- #
-
-parser.add_argument("--K", type=int, default=128, help="config for DiTGaussian")
 parser.add_argument("--mlp_type", type=str, default="mlp", help="config for DiTGaussian")
-
-parser.add_argument("--pos_embedder_test", type=int, default=4, help="config for DiTGaussian")
 
 # -------- Deepsets DiTGaussian args -------- #
 
@@ -271,18 +184,6 @@ parser.add_argument("--t_hidden_size", type=int, default=32, help="config for De
 
 parser.add_argument("--pos_emb_gamma_1_size", type=int, default=32, help="config for Deepsets")
 parser.add_argument("--gamma_1_hidden_size", type=int, default=32, help="config for Deepsets")
-
-# -------- sym_diff time args -------- #
-
-parser.add_argument("--enc_gnn_layers", type=int, default=2, help="config for DiTMessage")
-parser.add_argument("--enc_gnn_hidden_size", type=int, default=256, help="config for DiTMessage")
-
-# -------- sym_diff time args -------- #
-
-parser.add_argument("--num_bands", type=int, default=32, help="fourier time embedding config")
-parser.add_argument("--max_resolution", type=float, default=100, help="fourier time embedding config")
-parser.add_argument("--concat_t", action="store_true", help="fourier time embedding config")
-parser.add_argument("--t_fourier", action="store_true", help="time config for transformer")
 
 
 args = parser.parse_args()
@@ -295,7 +196,6 @@ atom_encoder = dataset_info['atom_encoder']
 atom_decoder = dataset_info['atom_decoder']
 
 args.cuda = not args.no_cuda and torch.cuda.is_available()
-#args.cuda = False
 device = torch.device("cuda" if args.cuda else "cpu")
 dtype = torch.float32
 

main_toy.py
@@ -119,8 +119,8 @@ parser.add_argument('--n_stability_samples', type=int, default=500,
 parser.add_argument('--normalize_factors', type=eval, default=[1, 4, 1],
                     help='normalize factors for [x, categorical, integer]')
 parser.add_argument('--remove_h', action='store_true')
-parser.add_argument('--include_charges', type=eval, default=True,
-                    help='include atom charge or not')
+parser.add_argument('--include_charges', type=eval, default=False,
+                    help='include atom charge or not')  # changed from True
 parser.add_argument('--visualize_every_batch', type=int, default=1e8,
                     help="Can be used to visualize multiple times per epoch")
 parser.add_argument('--normalization_factor', type=float, default=1,
@@ -128,9 +128,19 @@ parser.add_argument('--normalization_factor', type=float, default=1,
 parser.add_argument('--aggregation_method', type=str, default='sum',
                     help='"sum" or "mean"')
 
-# -------- distribution args -------- #
+# -------- retrieve_dataloaders args -------- #
 
+parser.add_argument("--toy_datasets_path", type=str, default="", help="where to save the toy datasets")
+parser.add_argument("--resample_toy_data", action="store_true", help="whether to resample the toy datasets")
+parser.add_argument("--toy_train_n", type=int, default=1000, help="toy datasets config")
+parser.add_argument("--toy_train_rotate", action="store_true", help="toy datasets config")
+parser.add_argument("--toy_val_n", type=int, default=1000, help="toy datasets config")
+parser.add_argument("--toy_test_n", type=int, default=500, help="toy datasets config")
 
+# -------- TestDistributions args -------- #
+
+parser.add_argument("--min_radius", type=float, default=0.5, help="TestDistribution config")
+parser.add_argument("--max_radius", type=float, default=0.8, help="TestDistribution config")
 
 # -------- sym_diff args -------- #
 
@@ -152,7 +162,7 @@ parser.add_argument("--mlp_ratio", type=float, default=2.0, help="config for DiT
 
 parser.add_argument("--x_emb", type=str, default="fourier", help="config for DiT")
 
-# -------- DiT_GNN and DiT_DiT args -------- #
+# -------- DiT_DiT args -------- #
 
 parser.add_argument("--enc_x_scale", type=float, default=25.0, help="config for DiT_GNN")
 parser.add_argument("--enc_hidden_size", type=int, default=64, help="config for DiT_GNN")
@@ -174,6 +184,7 @@ parser.add_argument("--mlp_dropout", type=float, default=0.0, help="config for D
 # -------- DiTGaussian args -------- #
 
 parser.add_argument("--K", type=int, default=128, help="config for DiTGaussian")
+parser.add_argument("--pos_embedder_test", type=int, default=32, help="config for DiTEmb")
 parser.add_argument("--mlp_type", type=str, default="mlp", help="config for DiTGaussian")
 
 # -------- Deepsets DiTGaussian args -------- #
@@ -188,8 +199,11 @@ parser.add_argument("--gamma_1_hidden_size", type=int, default=32, help="config 
 
 
 args = parser.parse_args()
+
+# args to ensure compatibility
 args.molecule = False
 args.n_dims = 2
+args.context_node_nf = 0
 
 args.cuda = not args.no_cuda and torch.cuda.is_available()
 device = torch.device("cuda" if args.cuda else "cpu")
@@ -265,7 +279,7 @@ def main():
             scheduler.load_state_dict(scheduler_state_dict)
 
     # Initialize dataparallel if enabled and possible.
-    if args.dp and torch.cuda.device_count() > 1:
+    if args.dp and torch.cuda.device_count() > 1 and False:  # remove for now
         print(f'Training using {torch.cuda.device_count()} GPUs')
         model_dp = torch.nn.DataParallel(model.cpu())
         model_dp = model_dp.cuda()
@@ -281,7 +295,7 @@ def main():
             ema_state_dict = torch.load(join('outputs', args.resume, 'generative_model_ema.npy'))
             model_ema.load_state_dict(ema_state_dict)
 
-        if args.dp and torch.cuda.device_count() > 1:
+        if args.dp and torch.cuda.device_count() > 1 and False:
             model_ema_dp = torch.nn.DataParallel(model_ema)  # used just for test
         else:
             model_ema_dp = model_ema

qm9/losses.py
@@ -24,7 +24,7 @@ def compute_loss_and_nll(args, generative_model, nodes_dist, x, h, node_mask, ed
 
         N = node_mask.squeeze(2).sum(1).long()  # [bs]
 
-        log_pN = nodes_dist.log_prob(N)
+        log_pN = nodes_dist.log_prob(N) if args.molecule else torch.zeros_like(nll)
 
         assert nll.size() == log_pN.size()
         nll = nll - log_pN  # include likelihood of the node number

sym_nn/distributions.py
@@ -13,8 +13,13 @@ class Distribution(ABC):
         pass
 
     def sample(self, n):
-        # return dict of positions, atom_mask, 
-        # edge_mask, one_hot
+        """
+        Return dict of:
+        positions: [n, 2, 2],
+        atom_mask: [n, 2, 1],
+        edge_mask: torch.empty(n),
+        one_hot: [n, 2, 1]
+        """
         raise NotImplementedError
 
 
@@ -27,9 +32,9 @@ class TestDistribution(Distribution):
 
     def sample(self, n, rotate=False):
 
-        radii = self.min_radius + torch.rand(n) * (
+        radii = self.min_radius + torch.rand(n, 1) * (
             self.max_radius - self.min_radius
-        ).unsqueeze(-1)
+        )
 
         # [n, 2]
         if rotate:
@@ -49,9 +54,9 @@ class TestDistribution(Distribution):
             dim=1
             )
 
-        atom_mask = torch.ones(n, 2, 2)
-        edge_mask = torch.empty(n)
-        one_hot = torch.zeros(n, 2, 2)
+        atom_mask = torch.ones(n, 2)
+        edge_mask = torch.empty(n, 2, 2)
+        one_hot = torch.zeros(n, 2, 1)
 
         return {"positions": positions, "atom_mask": atom_mask, 
                 "edge_mask": edge_mask, "one_hot": one_hot}
@@ -88,7 +93,7 @@ def retrieve_dataloaders(args):
         val_dataset = torch.load(os.path.join(toy_dataset_path, "valid.pt"))
         test_dataset = torch.load(os.path.join(toy_dataset_path, "test.pt"))
     else:
-        distribution = retrieve_dataloaders(args)
+        distribution = retrieve_distribution(args)
         train_dataset = ProcessedDataset(distribution.sample(args.toy_train_n, rotate=args.toy_train_rotate))
         val_dataset = ProcessedDataset(distribution.sample(args.toy_val_n))
         test_dataset = ProcessedDataset(distribution.sample(args.toy_test_n))

sym_nn/sym_nn.py
@@ -35,21 +35,24 @@ class DiTGaussian_dynamics(nn.Module):
         super().__init__()
 
         self.args = args
+        self.molecule = args.molecule
+
         self.n_dims = n_dims
+        self.in_node_nf = in_node_nf if self.molecule else 0
 
         self.mlp_type = mlp_type
 
         self.gaussian_embedder = GaussianLayer(K=K)
 
         self.model = DiT(
-            out_channels=n_dims+in_node_nf+context_node_nf, x_scale=0.0, 
+            out_channels=n_dims+self.in_node_nf+context_node_nf, x_scale=0.0, 
             hidden_size=hidden_size, depth=depth, 
             num_heads=num_heads, mlp_ratio=mlp_ratio, 
             use_fused_attn=True, x_emb="identity", mlp_dropout=mlp_dropout,
             mlp_type=mlp_type
             )
 
-        self.xh_embedder = nn.Linear(n_dims+in_node_nf+context_node_nf, xh_hidden_size)
+        self.xh_embedder = nn.Linear(n_dims+self.in_node_nf+context_node_nf, xh_hidden_size)
         self.pos_embedder = nn.Linear(K, hidden_size-xh_hidden_size)
 
         def _basic_init(module):
@@ -66,6 +69,13 @@ class DiTGaussian_dynamics(nn.Module):
     def forward(self):
         raise NotImplementedError
 
+    def ve_scaling(self, gamma_t):
+        c_in = torch.sqrt(
+            self.args.sigma_data**2 +
+            torch.exp(gamma_t[1]).unsqueeze(-1)
+        )
+        return c_in
+
     def _forward(self, t, xh, node_mask, edge_mask, context):
         # t: [bs, 1]
         # xh: [bs, n_nodes, dims]
@@ -78,7 +88,14 @@ class DiTGaussian_dynamics(nn.Module):
         x = xh[:, :, :self.n_dims]
         h = xh[:, :, self.n_dims:]
         x = remove_mean_with_mask(x, node_mask)
-        xh = torch.cat([x.clone(), h], dim=-1)
+
+        bs, n_nodes, h_dims = h.shape
+
+        if self.molecule:
+            xh = torch.cat([x.clone(), h], dim=-1)
+        else:
+            xh = x.clone()
+
         xh = self.xh_embedder(xh)  # [bs, n_nodes, xh_hidden_size]
 
         N = torch.sum(node_mask, dim=1, keepdims=True)  # [bs, 1, 1]
@@ -89,7 +106,7 @@ class DiTGaussian_dynamics(nn.Module):
         xh = node_mask * self.model(xh, t.squeeze(-1), node_mask.squeeze(-1))
 
         x = xh[:, :, :self.n_dims] 
-        h = xh[:, :, self.n_dims:]
+        h = xh[:, :, self.n_dims:] if self.molecule else torch.zeros(bs, n_nodes, h_dims, device=h.device)
 
         if self.args.com_free:
             x = remove_mean_with_mask(x, node_mask)  # k: U -> U
@@ -149,12 +166,15 @@ class DiT_DitGaussian_dynamics(nn.Module):
         super().__init__()
 
         self.args = args
+        self.molecule = args.molecule
+
         self.n_dims = n_dims
+        self.in_node_nf = in_node_nf if self.molecule else 0
 
         self.gaussian_embedder = GaussianLayer(K=K)
         self.gaussian_embedder_test = GaussianLayer(K=K)
 
-        self.xh_embedder = nn.Linear(n_dims+in_node_nf+context_node_nf, xh_hidden_size)
+        self.xh_embedder = nn.Linear(n_dims+self.in_node_nf+context_node_nf, xh_hidden_size)
         self.pos_embedder = nn.Linear(K, hidden_size-xh_hidden_size)
 
         self.pos_embedder_test = nn.Linear(K, pos_embedder_test)
@@ -165,7 +185,7 @@ class DiT_DitGaussian_dynamics(nn.Module):
         self.enc_concat_h = enc_concat_h  # NOTE: not used for now
 
         if enc_concat_h:
-            self.gamma_enc_input_dim = n_dims + in_node_nf + hidden_size-xh_hidden_size + noise_dims
+            self.gamma_enc_input_dim = n_dims + self.in_node_nf + hidden_size-xh_hidden_size + noise_dims
         else:
             self.gamma_enc_input_dim = n_dims + pos_embedder_test + noise_dims
 
@@ -179,14 +199,14 @@ class DiT_DitGaussian_dynamics(nn.Module):
             mlp_type=mlp_type
         ).to(device)
 
-        # add t emb here
+        # add t emb here?
         self.gamma_dec = Mlp(
             in_features=enc_hidden_size, hidden_features=dec_hidden_features,
             out_features=n_dims**2
         ).to(device)
 
-        self.model = DiT(
-            out_channels=n_dims+in_node_nf+context_node_nf, x_scale=0.0, 
+        self.backbone = DiT(
+            out_channels=n_dims+self.in_node_nf+context_node_nf, x_scale=0.0, 
             hidden_size=hidden_size, depth=depth, 
             num_heads=num_heads, mlp_ratio=mlp_ratio, 
             use_fused_attn=True, x_emb="identity", mlp_dropout=mlp_dropout,
@@ -252,13 +272,30 @@ class DiT_DitGaussian_dynamics(nn.Module):
         pos_emb = self.gaussian_embedder(x.clone(), node_mask)  # [bs, n_nodes, n_nodes, K]
         pos_emb = torch.sum(self.pos_embedder(pos_emb), dim=-2) / N  # [bs, n_nodes, hidden_size-xh_hidden_size]
 
-        xh = self.xh_embedder(torch.cat([x, h], dim=-1))
-        xh = node_mask * torch.cat([xh, pos_emb], dim=-1)
-        xh = node_mask * self.backbone(xh, t.squeeze(-1), node_mask.squeeze(-1))
+        if self.molecule:
+            xh = self.xh_embedder(torch.cat([x, h], dim=-1))
+            xh = node_mask * torch.cat([xh, pos_emb], dim=-1)
+            xh = node_mask * self.backbone(xh, t.squeeze(-1), node_mask.squeeze(-1))
+        else:
+            bs, n_nodes, h_dims = h.shape
+            x_ = self.xh_embedder(x)
+            x_ = node_mask * torch.cat([x_, pos_emb], dim=-1)
+            xh = node_mask * torch.cat(
+                [self.backbone(x_, t.squeeze(-1), node_mask.squeeze(-1)),
+                 torch.zeros(bs, n_nodes, h_dims, device=h.device)],
+                 dim=-1
+            )
 
         return xh
+    
+    def ve_scaling(self, gamma_t):
+        c_in = torch.sqrt(
+            self.args.sigma_data**2 +
+            torch.exp(gamma_t[1]).unsqueeze(-1)
+        )
+        return c_in
 
-    def _forward(self, t, xh, node_mask, edge_mask, context, gamma=None):
+    def _forward(self, t, xh, node_mask, edge_mask, context, gamma_t=None): 
         # t: [bs, 1]
         # xh: [bs, n_nodes, dims]
         # node_mask: [bs, n_nodes, 1]
@@ -266,7 +303,7 @@ class DiT_DitGaussian_dynamics(nn.Module):
         # return [bs, n_nodes, dims]
 
         assert context is None
-        assert gamma is None
+        #assert gamma_t is None
 
         bs, n_nodes, _ = xh.shape
 
@@ -276,12 +313,11 @@ class DiT_DitGaussian_dynamics(nn.Module):
         x = remove_mean_with_mask(x, node_mask)
 
         if not self.args.com_free:
-            assert gamma is not None
+            assert gamma_t is not None
             # Karras et al. (2022) scaling
-            x = x / torch.sqrt(
-                self.args.sigma_data**2 + 
-                torch.exp(gamma[1]).unsqueeze(-1)
-                )
+            c_in = self.ve_scaling(gamma_t)
+            x = x / c_in
+            h = h / c_in
 
         gamma = self.gamma(t, x, node_mask)
 